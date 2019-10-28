1c691efb309334c47b849d290c72828c2d775617
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-10-28 15:34:59 -0400

    working on styling the page

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
 var timeSlot = document.getElementById("#time");
 var inputSlot = document.getElementById("#input");
 var buttonSlot = document.getElementById("#button");
+var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
+
+$("#current").text(currentDateAndTime);
+console.log(currentDateAndTime); 
