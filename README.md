26d6875d6ac8e2e335daceea9d17e7830a0f54e5
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-11-08 13:50:15 -0500

    finial push

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# dayplanner
+# Day Planner Project
+    The purpose of this project is to give the user the utility of planning their day and keep track of time
+# Motivation
+    The goal of completing this project was to learn more about Third Party APIs
+# What I Learned
+    I learned how to use and manipulate an API.
+# Conclusion
+    This piece of home work wasn't as difficult as the others. It was all down to weither or not I could get the right information from the API.
+# Link
+    https://thoughtsinflight.github.io/dayplanner/
+# Image
+    ![Password Generator](https://i.imgur.com/RpGKYIb.png)
--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,49 @@ var timeSlot = document.getElementById("#time");
 var inputSlot = document.getElementById("#input");
 var buttonSlot = document.getElementById("#button");
 var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
+var now = moment().format('HH')
 
 $("#current").text(currentDateAndTime);
-console.log(currentDateAndTime); 
+// console.log(currentDateAndTime); 
+
+function coloringBox (time) {
+
+  if (now < time) {
+    $("#"+time).removeClass("bg-danger bg-secondary");
+    $("#"+time).addClass("bg-success");
+  }
+  else if (now > time){
+    $("#"+time).removeClass("bg-success bg-danger");
+    $("#"+time).addClass("bg-secondary");
+  }
+  else if (now = time){
+    $("#"+time).removeClass("bg-success bg-secondary");
+    $("#"+time).addClass("bg-danger");
+  }
+}
+coloringBox(9);
+coloringBox(10);
+coloringBox(11);
+coloringBox(12);
+coloringBox(13);
+coloringBox(14);
+coloringBox(15);
+coloringBox(16);
+coloringBox(17);
+
+
+// event delagation
+$(document).on("click", ".button", storeInput);
+
+function storeInput() {
+
+  const buttonClicked =$(this).attr("data-num");
+  const inputVal = $('#'+buttonClicked).val()
+ localStorage.setItem(buttonClicked, inputVal)
+}
+for (let i = 9; i < 18; i++) {
+  $('#'+i).val(localStorage.getItem(i))
+
+  
+}
+
